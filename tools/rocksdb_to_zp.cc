e2dfe6136e0cc761c65fb6cd8bcb2819ba5be250
CatKang <beijingwangkang@hotmail.com>
2017-07-26 19:53:56 +0800

    add rocksdb_to_zp tool

---
+++ tools/rocksdb_to_zp.cc
@@ -0,0 +1,88 @@
+#include <cassert>
+#include "rocksdb/db.h"
+
+#include "slash/include/env.h"
+#include "libzp/include/zp_cluster.h"
+
+void usage() {
+  std::cout << "usage:\n"
+    << "      ./rocksdb_to_zp db_path meta_ip meta_port table_name\n";
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 5) {
+    usage();
+    return -1;
+  }
+  std::string db_path(argv[1]);
+  std::string meta_ip(argv[2]);
+  int meta_port = atoi(argv[3]);
+  std::string table_name(argv[4]);
+
+  if (!slash::FileExists(db_path)) {
+    std::cout << "Db not exist: " << db_path << std::endl;
+    usage();
+    return -1;
+  }
+
+  // Connect zp
+  libzp::Options option;
+  libzp::Node node(meta_ip, meta_port);
+  option.meta_addr.push_back(node);
+  libzp::Cluster *cluster = new libzp::Cluster(option);
+  libzp::Status s = cluster->Connect();
+  if (!s.ok()) {
+    std::cout << "connect meta server failed! meta_ip: " << meta_ip
+      << ", meta_port: " << meta_port
+      << ", Error: "<< s.ToString() << std::endl;
+    delete cluster;
+    return -1;
+  }
+
+  // Open rocksdb
+  rocksdb::DB* db;
+  rocksdb::Options options;
+  rocksdb::Status status = rocksdb::DB::Open(options, db_path, &db);
+  if (!status.ok()) {
+    std::cout << "Failed to open db: " << db_path
+      << ", error: " << status.ToString() << std::endl;
+    delete cluster;
+    return -1;
+  }
+
+  std::cout << "Import data from rocksdb, db:" << db_path
+    << ", meta_ip: " << meta_ip << ", meta_port: " << meta_port
+    << ", table_name: " << table_name << ". y or n?"<< std::endl;
+  char c;
+  std::cin >> c;
+  if (c != 'y') {
+    delete cluster;
+    delete db;
+    return -1;
+  }
+
+  // Iterator all key value and write into zp
+  rocksdb::Iterator* it = db->NewIterator(rocksdb::ReadOptions());
+  for (it->SeekToFirst(); it->Valid(); it->Next()) {
+    s = cluster->Set(table_name, it->key().ToString(), it->value().ToString());
+    if (!s.ok()) {
+      std::cout << "set key " << it->key().ToString() << " failed: "
+        << s.ToString() << std::endl;
+      delete cluster;
+      delete db;
+      return -1;
+    }
+    std::cout << "set key " << it->key().ToString() << " success." << std::endl;
+  }
+  if (it->status().ok()) {
+    std::cout << "Success" << std::endl;
+  } else {
+    std::cout << "Failed" << it->status().ToString() << std::endl;
+  }
+  
+  delete it;
+  delete db;
+  delete cluster;
+  return 0;
+}
+
